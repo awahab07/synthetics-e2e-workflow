c70ad9c31d8adaa519360ba1b1b595f3775774c3
Abdul Zahid <awahab07@yahoo.com>
2023-04-12 16:44:32 +0200

    skip cookie banner click

--- journeys/recorded.journey.js
+++ journeys/recorded.journey.js
@@ -3,7 +3,7 @@ const { journey, step, expect } = require('@elastic/synthetics');
 journey('Recorded journey - Edited', async ({ page, context }) => {
   step('Go to https://www.elastic.co/', async () => {
     await page.goto('https://www.elastic.co/');
-    await page.locator('button[role="button"]:has-text("Accept")').click();
+    // await page.locator('button[role="button"]:has-text("Accept")').click();
   });
   step('Click [aria-label="Elastic Menu"] >> text=PlatformUse casesPricingCustomersResourcesCompany >> #platform', async () => {
     await page.locator('[aria-label="Elastic Menu"] >> text=PlatformUse casesPricingCustomersResourcesCompany >> #platform').click();
